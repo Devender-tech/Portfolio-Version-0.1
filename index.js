e10fbae9c36e16a1bf4ccd9e1becc96e181d57b7
Devender-tech <devender.tech97@gmail.com>
2020-07-11 20:29:40 +0530

    Improvements

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ document.querySelector('.menu-toggle').addEventListener('click',function(){
 
 document.querySelector('.mobile-menu').classList.toggle("active");
 });
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+    document.getElementsByTagName("header").classList.toggle('active');
+ 
+  
+}
